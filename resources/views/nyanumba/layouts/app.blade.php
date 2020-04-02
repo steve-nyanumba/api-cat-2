ae7b7134beae43a37a0b95f7d0881e4d7fa278c5
Steve Nyanumba <stevennmb9@gmail.com>
2020-04-02 21:11:10 +0300

    Added Secure_asset helper

--- resources/views/nyanumba/layouts/app.blade.php
+++ resources/views/nyanumba/layouts/app.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href=" {{ asset('css/app.css') }} ">
-    <script src=" {{ asset('js/app.js') }} "></script>
+    <link rel="stylesheet" href=" {{ secure_asset('css/app.css') }} ">
+    <script src=" {{ secure_asset('js/app.js') }} "></script>
     <title>API CAT 2</title>
 </head>
 <body>
